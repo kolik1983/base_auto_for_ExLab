8072c07ac93216d8a6bbc7d1703b113ed8701950
Alex ER <100380496+kolik1983@users.noreply.github.com>
2022-09-27 11:50:15 +0300

    Update base_page.py

--- pages/base_page.py
+++ pages/base_page.py
@@ -5,13 +5,12 @@ import argparse
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver import Remote as RemoteWebDriver
-from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
 from .all_locators.locators_for_test_lending_page import BaseLocators
 from colorama import init
-from  colorama  import  Fore ,  Back ,  Style
+from  colorama  import  Fore,  Style
 import time
 
 # Родительский класс
@@ -21,22 +20,18 @@ class BasePage():
         self.browser = browser
         self.url = url
 
-
     # Создаем метод открытия и перехода по ссылке page.open()
     def open(self):
         self.browser.delete_all_cookies()  # Удоляем все куки
         self.browser.get(self.url)
 
-
-    #  Если элемент найден, возвращаем True,
-    #  иначе - перехватываем ошибку 'NoSuchElementException'
-    #  и присваиваем False
     def is_element_present(self, how, what):
         try:
-            WebDriverWait(self.browser, 20).until(EC.presence_of_element_located((how, what)))
-        except (NoSuchElementException):
+            WebDriverWait(self, 20).until(EC.presence_of_element_located((how, what)))
+        except (TimeoutException):
             return False
         return True
+
 
 
     # Если элемент кликабельный, возвращаем True,
