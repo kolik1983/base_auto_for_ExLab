90929fd51f4a618587795ad2c48e0fd94abe61aa
Alex ER <100380496+kolik1983@users.noreply.github.com>
2022-09-27 11:48:23 +0300

    Update locators_for_test_lending_page.py

--- pages/all_locators/locators_for_test_lending_page.py
+++ pages/all_locators/locators_for_test_lending_page.py
@@ -1,4 +1,3 @@
-
 from selenium.webdriver.common.by import By
 
 class BaseLocators():
@@ -7,3 +6,5 @@ class BaseLocators():
 class HeadersLendingPageLocators():
     HEADER_LENDING = (By.XPATH, '//div[@id="header"]')
     LOGO_PIC_EXLAB = (By.XPATH, '//div[@class="sc-jqUVSM EnPDN"]')
+    HEADER_LINK_ABOUT = (By.XPATH, '//a[@data-scroll-to="#about"]')
+    HEADER_LINK_PROJECTS = (By.XPATH, '//a[@data-scroll-to="#projects"]')
