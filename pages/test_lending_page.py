681e57bed26beb5af27ed430e0ef549ebdd617ef
Alex ER <100380496+kolik1983@users.noreply.github.com>
2022-09-27 11:52:26 +0300

    Update test_lending_page.py

--- pages/test_lending_page.py
+++ pages/test_lending_page.py
@@ -1,16 +1,14 @@
+
+import pytest
 import time
-from .base_page import BasePage
 from lending.pages.all_locators.locators_for_test_lending_page import *
-from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.support.ui import WebDriverWait
-
-
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.support import expected_conditions as EC
+from .base_page import BasePage
 
 class TestLandingPage():
+    # Проверяем что страница лендинга загрузилась
     def test_open_my_page(self, browser):
         BasePage(browser, BaseLocators.LENDING_LINK).open()
-
-
-        status_logo_exlab = BasePage.is_element_present(self, *HeadersLendingPageLocators.HEADER_LENDING)
-        print(status_logo_exlab)
-        assert status_logo_exlab == True, 'Перехода на страницу лендинка не произошло'
+        assert  BasePage.is_element_present(browser, *HeadersLendingPageLocators.HEADER_LENDING) is True, 'Лендинг не загрузился, логотип ExLab не отображается'
