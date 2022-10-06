307b6523f87fc2df5f28a78a514629c286818125
Alex ER <100380496+kolik1983@users.noreply.github.com>
2022-10-07 00:09:36 +0300

    Update test_lending_page.py

--- pages/test_lending_page.py
+++ pages/test_lending_page.py
@@ -8,7 +8,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from .base_page import BasePage
 
 class TestLandingPage():
-    # Проверяем что страница лендинга загрузилась
+    #Проверяем что страница лендинга загрузилась
     def test_open_my_page(self, browser, open_url):
         assert  BasePage.is_element_present(browser, *HeadersLendingPageLocators.HEADER_LENDING), 'Лендинг не загрузился, логотип ExLab не отображается'
 
@@ -22,7 +22,7 @@ class TestLandingPage():
 
     def test_link_about(self, browser,open_url):
         # Проверка отображения пункта "О нас" в хедере
-        assert BasePage.is_visible(browser, *HeadersLendingPageLocators.HEADER_LINK_ABOUT), 'Ссылка "О нас" отсутсвует'
+        assert BasePage.is_visible(browser, *HeadersLendingPageLocators.HEADER_LINK_ABOUT), 'Пункт "О нас" отсутсвует'
 
     def test_about_us_click(self, browser, open_url):
         # Проверка кликабельности ссылки О нас
@@ -33,10 +33,31 @@ class TestLandingPage():
         browser.find_element(*HeadersLendingPageLocators.HEADER_LINK_ABOUT).click()
         assert BasePage.is_visible(browser, *HeadersLendingPageLocators.PARAGRAPH_ABOUT), 'Выбор элемента не ведет в соответсвующий пункт'
 
+    def test_link_project(self, browser, open_url):
+        # Проверка отображения пункта "Проекты"
+        assert BasePage.is_visible(browser, *HeadersLendingPageLocators.HEADER_LINK_PROJECTS), 'Пункт "Проекты" отсутствует'
 
+    def test_project_click(self, browser, open_url):
+        # Проверка кликабельности ссылки Проекты
+        assert BasePage.is_element_clickable(browser, *HeadersLendingPageLocators.HEADER_LINK_PROJECTS), 'Ссылка не кликабельна'
 
+    def test_click_on_the_link_project(self, browser, open_url):
+        # Проверка перехода по ссылке в соответсвующий пункт
+        browser.find_element(*HeadersLendingPageLocators.HEADER_LINK_PROJECTS).click()
+        assert BasePage.is_visible(browser, *HeadersLendingPageLocators.PARAGRAPH_PROJECT), 'Пункт "Проекты" отсутсвует'
 
+    def test_link_mentors(self, browser, open_url):
+        # Проверка отображения пункта "Менторы"
+        assert BasePage.is_visible(browser, *HeadersLendingPageLocators.HEADER_LINK_MENTORS), 'ПУНКТ "Менторы" отсутсвует'
 
+    def test_mentors_click(self, browser, open_url):
+        # Проверка кликабельности ссылки Менторы
+        assert BasePage.is_element_clickable(browser, *HeadersLendingPageLocators.HEADER_LINK_MENTORS), 'Ссылка не кликабельна'
+
+    def test_click_on_the_link_mentors(self, browser, open_url):
+        # Проверка перехода в пункт Менторы
+        browser.find_element(*HeadersLendingPageLocators.HEADER_LINK_MENTORS).click()
+        assert BasePage.is_visible(browser, *HeadersLendingPageLocators.PARAGRAPH_MENTORS), 'Переход в пункт  о менторах не осуществился'
 
 
 
