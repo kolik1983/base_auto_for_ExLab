c6c0306316a573258610a6720e537103f63a4ea2
Alex ER <100380496+kolik1983@users.noreply.github.com>
2022-10-12 00:17:08 +0300

    Update test_lending_page.py

--- pages/test_lending_page.py
+++ pages/test_lending_page.py
@@ -1,5 +1,7 @@
 import pytest
 import time
+import allure
+from allure_commons.types import AttachmentType
 import webbrowser
 from pages.all_locators.locators_for_test_lending_page import *
 from selenium.webdriver.support.ui import WebDriverWait
@@ -7,10 +9,10 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.support import expected_conditions as EC
 from .base_page import BasePage
 
-class TestLandingPage():
+class TestLanding():
     #Проверяем что страница лендинга загрузилась
     def test_open_my_page(self, browser, open_url):
-        assert  BasePage.is_element_present(browser, *HeadersLendingPageLocators.HEADER_LENDING), 'Лендинг не загрузился, логотип ExLab не отображается'
+        assert BasePage.is_element_present(browser, *HeadersLendingPageLocators.HEADER_LENDING), 'Лендинг не загрузился, логотип ExLab не отображается'
 
     def test_open_black_theme(self, browser, open_url):
         # Проверка что запущена темная тема, по переключателю который находится в положении включить светлую тему.
@@ -33,16 +35,23 @@ class TestLandingPage():
         browser.find_element(*HeadersLendingPageLocators.HEADER_LINK_ABOUT).click()
         assert BasePage.is_visible(browser, *HeadersLendingPageLocators.PARAGRAPH_ABOUT), 'Выбор элемента не ведет в соответсвующий пункт'
 
+    @allure.feature('Project_test')
+    @allure.story('Проверка отображения пункта "Проекты"')
     def test_link_project(self, browser, open_url):
         # Проверка отображения пункта "Проекты"
         assert BasePage.is_visible(browser, *HeadersLendingPageLocators.HEADER_LINK_PROJECTS), 'Пункт "Проекты" отсутствует'
 
+    @allure.feature('Project_test')
+    @allure.story('Проверка кликабельности ссылки Проекты')
     def test_project_click(self, browser, open_url):
         # Проверка кликабельности ссылки Проекты
         assert BasePage.is_element_clickable(browser, *HeadersLendingPageLocators.HEADER_LINK_PROJECTS), 'Ссылка не кликабельна'
 
+    @allure.feature('Project_test')
+    @allure.story('Проверка перехода по ссылке в соответсвующий пункт')
     def test_click_on_the_link_project(self, browser, open_url):
         # Проверка перехода по ссылке в соответсвующий пункт
+        # allure.attach(browser.get_full_page_screenshot_as_png(), name='Скрин', attachment_type=AttachmentType.PNG)
         browser.find_element(*HeadersLendingPageLocators.HEADER_LINK_PROJECTS).click()
         assert BasePage.is_visible(browser, *HeadersLendingPageLocators.PARAGRAPH_PROJECT), 'Пункт "Проекты" отсутсвует'
 
