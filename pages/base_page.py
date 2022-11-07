1d5be3dc975b8c89d86714d7ef6ba34b95fa14b0
Alex ER <100380496+kolik1983@users.noreply.github.com>
2022-11-08 01:13:45 +0300

    Update base_page.py

--- pages/base_page.py
+++ pages/base_page.py
@@ -8,6 +8,7 @@ from selenium.webdriver import Remote as RemoteWebDriver, ActionChains
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException, TimeoutException
+from selenium.webdriver.common.keys import Keys
 from .all_locators.locators_for_test_lending_page import BaseLocators
 from colorama import init
 from colorama import Fore, Style
@@ -22,7 +23,7 @@ class BasePage():
 
     # Создаем метод открытия и перехода по ссылке page.open()
     def open(self):
-        self.browser.delete_all_cookies()  # Удоляем все куки
+        self.browser.delete_all_cookies()  # Удаляем все куки
         self.browser.get(self.url)
 
     # def find(self, timeout=10):
@@ -50,6 +51,13 @@ class BasePage():
             return False
         return True
 
+    def is_visible_elements(self, how, what):
+        try:
+            WebDriverWait(self, 20).until(EC.visibility_of_all_elements_located((how, what)))
+        except TimeoutException:
+            return False
+        return True
+
     # Если элемент кликабельный, возвращаем True,
     # иначе - перехватываем ошибку 'NoSuchElementException'
     # и присваиваем False
@@ -61,20 +69,30 @@ class BasePage():
         return True
 
     def move_to_element(self, browser, how, what):
+        try:
         # Переход до нужного элемента
+            element = browser.find_element(how, what)
+            actions = ActionChains(browser)
+            actions.move_to_element(element).perform()
+        except NoSuchElementException:
+            return False
+        return True
 
-        element = browser.find_element(how, what)
-        actions = ActionChains(browser)
-        actions.move_to_element(element).perform()
 
     def scroll_to_element(self, browser, how, what):
+        try:
        # Скролл до элемента
-        element = browser.find_element(how, what)
-        browser.execute_script("arguments[0].scrollIntoView();", element)
+            element = browser.find_element(how, what)
+            browser.execute_script("arguments[0].scrollIntoView();", element)
+
+        except NoSuchElementException:
+            return False
+        return True
+
 
     def scroll_down(self, browser):
         # ?Скролл вниз
-        browser.execute_script("window.scrollBy(0,800)", "")
+        browser.execute_script("window.scrollTo(0,1000)")
 
 
     def get_current_url(self):
@@ -88,5 +106,33 @@ class BasePage():
         except TimeoutException:
             return False
         return True
+
+
+    def open_spoilers(self, browser, how, what):
+        try:
+
+            count_it = len(browser.find_elements(how, what))
+
+            for area in range(1, count_it + 1):
+                what_to_click = browser.find_element(how, f'({what})[{area}]')
+                browser.execute_script('return arguments[0].scrollIntoView();', what_to_click)
+                what_to_click.click()
+
+        except NoSuchElementException:
+            return False
+        return True
+
+    def scroll_to_futter(self, browser):
+
+        try:
+            browser.implicitly_wait(15)
+            footer = browser.find_element(By.TAG_NAME, 'html')
+            footer.send_keys(Keys.END)
+
+        except NoSuchElementException:
+            return False
+        return True
+
+
 
 
